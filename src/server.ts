9ebf1b1f601ea996c3e951c7224f3249c9ecf64f
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-02-23 17:25:37 +0200

    add middlewares for security and loggin

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+app.use(
+    rateLimit({
+        windowMs: 24 * 60 * 3, // next request to endpoint
+        max: 100, // maximal request for all endpoint
+        message: 'To many request, send back request after 3 minutes',
+    })
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello');
